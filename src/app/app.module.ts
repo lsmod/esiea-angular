93c936c7724bcd6d4a908002c41a3feb4737f597
lsmod <1526305+lsmod@users.noreply.github.com>
2024-04-30 15:54:22 +0200

    adds reactive form module example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Route } from '@angular/router';
 
 import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TvShowItemComponent } from './tv-show-item/tv-show-item.component';
 import { TvShowListComponent } from './tv-show-list/tv-show-list.component';
 import { TvShowDetailComponent } from './tv-show-detail/tv-show-detail.component';
@@ -19,6 +19,7 @@ import { routes } from './routes';
 import { NewShowComponent } from './new-show/new-show.component';
 import { NewShowPreviewComponent } from './new-show-preview/new-show-preview.component';
 import { NewShowTemplateFormComponent } from './new-show-template-form/new-show-template-form.component';
+import { NewShowReactiveFormComponent } from './new-show-reactive-form/new-show-reactive-form.component';
 
 @NgModule({
   declarations: [
@@ -30,16 +31,17 @@ import { NewShowTemplateFormComponent } from './new-show-template-form/new-show-
     NewShowComponent,
     NewShowPreviewComponent,
     NewShowTemplateFormComponent,
+    NewShowReactiveFormComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot(routes),
     BrowserAnimationsModule,
     MatChipsModule,
     MatCardModule,
+    ReactiveFormsModule,
     MatButtonModule,
     MatGridListModule,
     CdkAccordionModule,
---
+++ src/app/new-show-reactive-form/new-show-reactive-form.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-show-reactive-form',
+  templateUrl: './new-show-reactive-form.component.html',
+  styleUrls: ['./new-show-reactive-form.component.css'],
+})
+export class NewShowReactiveFormComponent implements OnInit {
+  description = new FormControl('new show description');
+
+  newShowGroup = new FormGroup({
+    title: new FormControl('titre', Validators.required),
+    description: new FormControl(
+      'new show description',
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(100),
+      ])
+    ),
+  });
+
+  ngOnInit(): void {
+    this.newShowGroup.valueChanges.subscribe((value) => {
+      console.log('new title: ', value.title);
+      console.log('new description:', value.description);
+    });
+  }
+
+  onSubmit() {
+    const value = this.newShowGroup.value;
+    console.log('title', value.title);
+    console.log('description', value.description);
+
+    // TODO : POST ON /api/tv-shows
+  }
+}
